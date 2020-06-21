18fe7e2d21007d5d08a965e6dd5bce85d047792c
João Victor Cruz <50991788+dev-cruz@users.noreply.github.com>
2020-06-21 20:25:24 -0300

    Created the layout for chats page (#7)

--- lib/live_chat_web/live/page_live.ex
+++ lib/live_chat_web/live/page_live.ex
@@ -3,46 +3,14 @@ defmodule LiveChatWeb.PageLive do
 
   @impl true
   def mount(_params, %{"user" => user}, socket) do
-    socket = assign(socket, query: "", results: %{}, user: user, num: 0)
-    IO.puts connected?(socket)
+    socket = assign(socket, user: user, chats: nil)
     {:ok, socket}
   end
 
   @impl true
-  def handle_event("suggest", %{"q" => query}, socket) do
-    {:noreply, assign(socket, results: search(query), query: query)}
-  end
+  def handle_event("search_user", %{"query" => query}, socket) do
+    IO.puts query
 
-  @impl true
-  def handle_event("search", %{"q" => query}, socket) do
-    case search(query) do
-      %{^query => vsn} ->
-        {:noreply, redirect(socket, external: "https://hexdocs.pm/#{query}/#{vsn}")}
-
-      _ ->
-        {:noreply,
-         socket
-         |> put_flash(:error, "No dependencies found matching \"#{query}\"")
-         |> assign(results: %{}, query: query)}
-    end
-  end
-
-  def handle_event("add", _value, socket) do
-    new_num = socket.assigns.num + 1
-
-    socket = assign(socket, num: new_num)
     {:noreply, socket}
-  end
-
-  defp search(query) do
-    if not LiveChatWeb.Endpoint.config(:code_reloader) do
-      raise "action disabled when not in development"
-    end
-
-    for {app, desc, vsn} <- Application.started_applications(),
-        app = to_string(app),
-        String.starts_with?(app, query) and not List.starts_with?(desc, ~c"ERTS"),
-        into: %{},
-        do: {app, vsn}
   end
 end
