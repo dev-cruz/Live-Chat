447c9ca213f50b7ebb4965971e08b257a594d2ac
joaocruz19 <joaovictorcruz82@gmail.com>
2020-07-18 18:11:15 -0300

    Revert "Created the migrations for chats creation"
    This reverts commit 277ccec4ce37fe636dfdbd5e669a963e480e6272.

--- lib/live_chat/chats/chats.ex
+++
@@ -1,7 +0,0 @@
-defmodule LiveChat.Chats do
-  import Ecto.Query, warn: false
-
-  alias LiveChat.Chats.Message
-  alias LiveChat.Chats.Chatroom
-
-end
--- priv/repo/migrations/20200718180227_add_message_chatroom.exs
+++
@@ -1,9 +0,0 @@
-defmodule LiveChat.Repo.Migrations.AddMessageChatroom do
-  use Ecto.Migration
-
-  def change do
-    alter table(:chatrooms) do
-      add :message_id, references(:messages)
-    end
-  end
-end
